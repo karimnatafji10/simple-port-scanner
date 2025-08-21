23fc308e2f1a4fc1b9f940caec695fe377db258f
karimnatafji10 <karim.natafji@torontomu.ca>
2025-08-20 22:11:43 -0400

    Create simple_port_scanner.py

---
+++ simple_port_scanner.py
@@ -0,0 +1,22 @@
+# simple_port_scanner.py
+import socket
+
+# Target IP address (example: google.com)
+target = ""   # "localhost" (your own computer)
+
+# Range of ports to scan
+ports = [21, 22, 80, 443, 3306]
+
+print(f"Scanning {target}...")
+
+for port in ports:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)  # 1 second timeout
+    result = sock.connect_ex((target, port))
+    
+    if result == 0:
+        print(f"Port {port} is OPEN")
+    else:
+        print(f"Port {port} is CLOSED")
+    
+    sock.close()
